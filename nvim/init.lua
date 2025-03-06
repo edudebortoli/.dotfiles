efeb20b9921d91f6272e6cb25a3e8e61bb858e1a
Eduardo <edudebortoli@icloud.com>
2025-03-06 10:21:35 -0300

    back to lazy as plugin manager

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,26 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+--
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
+require("eduardo")
